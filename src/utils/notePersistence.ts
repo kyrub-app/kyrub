df20dd778113ed1c71cec0bf574a731eb69c28a4
kyrub-app <kyrub.app@gmail.com>
2026-07-24 04:19:16 -0300

    chore: remove unused note persistence wrapper

--- src/utils/notePersistence.ts
+++
@@ -1,8 +0,0 @@
-import { deleteDoc, doc } from 'firebase/firestore';
-import { db } from './firebase';
-
-export const deleteOwnedCloudNote = (
-  ownerId: string,
-  noteId: string
-): Promise<void> =>
-  deleteDoc(doc(db, 'users', ownerId, 'tasks', noteId));
