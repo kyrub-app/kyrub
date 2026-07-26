f649060a568d63c8c3eab5b7aceb4d5816ef448b
kyrub-app <kyrub.app@gmail.com>
2026-07-26 04:23:22 -0300

    Load catalog category tree layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './styles/responsive-product-cards.css';
+import './styles/catalog-category-tree.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
