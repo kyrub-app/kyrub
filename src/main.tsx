04f49ec2c3807f50b3cd9d41c723198b27aa90ad
kyrub-app <kyrub.app@gmail.com>
2026-09-21 22:47:54 -0300

    fix(integrations): retire DOM-injected fiscal homologation bridge

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import { OfficialKnowledgeSemanticSetupBridge } from './components/OfficialKnowl
 import { OfficialKnowledgeSetupBridge } from './components/OfficialKnowledgeSetupBridge';
 import { PdvProductQuickActionsBridge } from './components/pdv/PdvProductQuickActionsBridge';
 import { BuyerPickupCodeBridge } from './components/store/BuyerPickupCodeBridge';
-import { FiscalHomologationTabBridge } from './components/store/FiscalHomologationTabBridge';
 import { MercadoLivreOAuthReturnBridge } from './components/store/MercadoLivreOAuthReturnBridge';
 import { MercadoPagoReceivablesBridge } from './components/store/MercadoPagoReceivablesBridge';
 import { PickupPdvNavigationBridge } from './components/store/PickupPdvNavigationBridge';
@@ -56,7 +55,6 @@ createRoot(rootElement).render(
           <PickupPdvNavigationBridge />
           <PdvProductQuickActionsBridge />
           <GerencialIntegrationsNativeBridge />
-          <FiscalHomologationTabBridge />
           <MercadoPagoReceivablesBridge />
           <StoreOwnedPixReceivablesBridge />
           <AdminMercadoPagoOAuthBridge />
